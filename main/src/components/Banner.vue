17f46848392972ec2bf0d2873bb026daf0e760f5
Wahyu Pranata <wahyupranata131@gmail.com>
2022-03-27 21:03:27 +0800

    create new component for main page

---
+++ main/src/components/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-[url('../src/assets/suisnbuna.png')] bg-top-center w-full h-64 relative mb-28">
+    <div class="absolute inset-0 bg-white/5 backdrop-blur-md"></div>
+    <div class="absolute left-1/2 -translate-x-1/2 -bottom-24">
+      <img src="../assets/yorulogo.jpg" alt="yorushika" class="rounded-xl w-48 shadow-md  mx-auto">
+      <blockquote class="font-notoserifjp text-lg text-center mt-2 tracking-widest text-yoru-300">A Tribute Page</blockquote>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
