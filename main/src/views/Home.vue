e43776867f42b84ef62ad57ec38cd6aff2379294
Wahyu Pranata <wahyupranata131@gmail.com>
2022-03-31 21:08:31 +0800

    add vue router

---
+++ main/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+      <Banner/>
+      <Introduction id="about"/>
+    </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import Introduction from '../components/Introduction.vue'
+export default {
+  components: {
+    Banner, Introduction
+  }
+}
+</script>
+
+<style>
+
+</style>
