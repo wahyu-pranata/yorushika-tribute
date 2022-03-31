a8f64ee577ef576f81e82fdc3f7154d1c9df324f
Wahyu Pranata <wahyupranata131@gmail.com>
2022-03-31 21:12:10 +0800

    change serif font

--- main/src/components/Banner.vue
+++ main/src/components/Banner.vue
@@ -3,7 +3,7 @@
     <div class="absolute inset-0 bg-white/5 backdrop-blur-md"></div>
     <div class="absolute left-1/2 -translate-x-1/2 -bottom-24">
       <img src="../assets/yorulogo.jpg" alt="yorushika" class="rounded-xl w-48 shadow-md  mx-auto">
-      <blockquote class="font-notoserifjp text-lg text-center mt-2 tracking-widest text-yoru-300">A Tribute Page</blockquote>
+      <blockquote class="font-lora text-lg text-center mt-2 tracking-widest text-yoru-300">A Tribute Page</blockquote>
     </div>
   </div>
 </template>
