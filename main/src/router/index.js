d8b3a75d001d0676c30868cac319a1cea02866e1
Wahyu Pranata <wahyupranata131@gmail.com>
2022-04-15 20:59:56 +0800

    create new routes

--- main/src/router/index.js
+++ main/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Biography from '../views/Biography.vue'
+import Discography from '../views/Discography.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/biography',
+    name: 'Biography',
+    component: Biography
+  },
+  {
+    path: '/disco',
+    name: 'Discography',
+    component: Discography
   }
 ]
 
