df361991c4797ccf4b6b075e7625d7a72889aed3
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-24 19:17:39 +0800

    add new route for discography details

--- main/src/router/index.js
+++ main/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Biography from '../views/Biography.vue'
 import Discography from '../views/Discography.vue'
+import DiscoDetails from '../views/Details/Discography.vue'
 
 const routes = [
   {
@@ -18,6 +19,12 @@ const routes = [
     path: '/disco',
     name: 'Discography',
     component: Discography
+  },
+  {
+    path: '/disco/:id',
+    name: 'Discography Details',
+    component: DiscoDetails,
+    props: true
   }
 ]
 
