135a9f460bae31ec25f20f2dd60415481b529c69
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-24 19:20:12 +0800

    add new content in home page

--- main/src/components/Introduction.vue
+++ main/src/components/Introduction.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="pt-6 flex flex-col items-center h-screen">
+  <div class="pt-6 flex flex-col items-center">
     <h1 class="heading">Introduction</h1>
     <div class="content-container">
       <div class="content">
         <h2 class="content-heading">Who is Yorushika?</h2>
-        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis adipisci dolor excepturi saepe quod hic quasi alias est dolores, ea dolorem porro tempore soluta debitis commodi illo aliquam illum nulla iusto? Omnis repudiandae reprehenderit tempore quibusdam iusto eum dolore porro dolor est incidunt dolorem quod, a, corporis, aliquam ex!
-          <router-link to="/about" class="link">Hello</router-link>
+        <p class="content-paragraph">
+          Yorushika (ヨルシカ) is a Japanese rock duo debuted in 2017, composed of two amazing and secretive members: N-buna, a former vocaloid music producer and Suis, a female vocalist. They haven't revealed their faces in the public nor the media, but they held 2 concerts, in July 2017 and August 2019.
         </p>
+        <p class="content-paragraph">
+          The name "Yorushika" was taken from a lyric in their song "Kumo to Yūrei" (The Clouds and the Ghost), the line "Yoru shika mō nemurezu ni" which translates to, "I can only sleep at night." Their logo mark is a motif of two moons facing each other and also serves as a clock hand, portraying the time "from 6:00 to night".
+        </p>
+          <router-link to="/about" class="link">Read Yorushika Biography</router-link>
       </div>
       <div class="content">
         <h2 class="content-heading">Yorushika Works</h2>
-        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis adipisci dolor excepturi saepe quod hic quasi alias est dolores, ea dolorem porro tempore soluta debitis commodi illo aliquam illum nulla iusto? Omnis repudiandae reprehenderit tempore quibusdam iusto eum dolore porro dolor est incidunt dolorem quod, a, corporis, aliquam ex!</p>
-      </div>
-      <div class="content">
-        <h2 class="content-heading">My Opinion on Yorushika?</h2>
-        <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis adipisci dolor excepturi saepe quod hic quasi alias est dolores, ea dolorem porro tempore soluta debitis commodi illo aliquam illum nulla iusto? Omnis repudiandae reprehenderit tempore quibusdam iusto eum dolore porro dolor est incidunt dolorem quod, a, corporis, aliquam ex!</p>
+        <ul class="list-container">
+          <li v-for="album in albums" key="album.id" class="content-list">{{ album.jpName }} - <span>{{ album.enName }}</span></li>
+        </ul>
+        <router-link to="/disco" class="link">View More!</router-link>
       </div>
     </div>
   </div>
@@ -22,6 +25,22 @@
 
 <script>
 export default {
+  data() {
+    return {
+      disco: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/discography')
+      .then(res => res.json())
+      .then(data => this.disco = data)
+      .catch(err => console.log(err.message))
+  },
+  computed: {
+    albums() {
+      return this.disco.filter(item => item.type === "Album" || item.type === "EP" || item.type === "Mini Album");
+    }
+  }
 }
 </script>
 
@@ -41,7 +60,16 @@ export default {
 .content-paragraph {
   @apply font-nunitosans first-letter:ml-8 mt-2
 }
+.list-container {
+  @apply space-y-2 ml-4 mt-2
+}
+.content-list {
+  @apply font-nunitosans text-yoru-400
+}
+.content-list span {
+  @apply text-yoru-300
+}
 .link {
-  @apply text-yoru-300 underline decoration-yoru-300 after:contents
+  @apply block bg-yoru-300 text-white px-3 py-1 mt-2 mx-auto w-max rounded-sm shadow-md hover:bg-yoru-200 hover:scale-[0.99] hover:shadow-none
 }
 </style>
