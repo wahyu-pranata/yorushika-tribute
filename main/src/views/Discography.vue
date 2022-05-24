fd515ff8b0bd8ad7f283a2c0e19af4abb410c00b
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-24 19:18:39 +0800

    add discographies details

--- main/src/views/Discography.vue
+++ main/src/views/Discography.vue
@@ -1,13 +1,22 @@
 <template>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quia.</h1>
+  <div class="main">
+    <Banner message="Discography" logo="src/assets/yorulogo.jpg"/>
+    <Disco />
+  </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
+import Disco from "../components/Discographies.vue"
 export default {
-
+  components:{
+    Banner, Disco
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .main {
+    @apply -z-10
+  }
 </style>
