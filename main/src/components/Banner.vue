596ea6c11ef7d35569be170cca1a6aafad2c44d2
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-24 19:12:50 +0800

    add props

--- main/src/components/Banner.vue
+++ main/src/components/Banner.vue
@@ -2,18 +2,21 @@
   <div class="bg-[url('../src/assets/suisnbuna.png')] bg-top-center w-full h-64 relative mb-28">
     <div class="absolute inset-0 bg-white/5 backdrop-blur-md"></div>
     <div class="absolute left-1/2 -translate-x-1/2 -bottom-24">
-      <img src="../assets/yorulogo.jpg" alt="yorushika" class="rounded-xl w-48 shadow-md  mx-auto">
-      <blockquote class="font-lora text-lg text-center mt-2 tracking-widest text-yoru-300">A Tribute Page</blockquote>
+      <img :src="logo" alt="yorushika" class="rounded-xl w-48 shadow-md mx-auto">
+      <div class="text-center">
+        <p class="font-lora text-xl mt-2 tracking-widest text-yoru-300">{{ message }}</p>
+        <small class="font-nunitosans w-full tracking-widest text-yoru-400">{{ sub }}</small>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  props: ['message', 'logo', 'sub']
 }
 </script>
 
-<style>
+<style >
 
 </style>
