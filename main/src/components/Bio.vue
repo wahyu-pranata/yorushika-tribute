4d7f833dd4d24f6d5788e9f8ebba692e5025e0f4
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-29 22:42:22 +0800

    add biography component

---
+++ main/src/components/Bio.vue
@@ -0,0 +1,46 @@
+<template>
+    <div v-if="biographies.length">
+        <div class="bio-container">
+            <div class="bio" v-for="bio in biographies" :key="bio.id">
+                <h3 class="bio-year">{{ bio.year }}</h3>
+                <ul class="bio-detail">
+                    <li v-for="detail in bio.details" :key="detail" class="bio-detail">
+                        {{ detail }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <p v-else>
+        Loading Biography Data...
+    </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            biographies: []
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/biography')
+        .then(res => res.json())
+        .then(data => this.biographies = data)
+        .catch(err => console.log(err.message))
+    }
+}
+</script>
+
+<style scoped>
+.bio-container {
+    @apply  max-w-3xl mx-auto space-y-4
+}
+.bio-year {
+    @apply font-lora text-yoru-300 text-xl tracking-wider font-bold
+}
+.bio-detail {
+    @apply font-nunitosans text-yoru-400 ml-3
+}
+
+</style>
--- main/src/views/Biography.vue
+++ main/src/views/Biography.vue
@@ -1,13 +1,22 @@
 <template>
-  
+    <div class="main">
+        <Banner message="Biography"/>
+        <Bio/>
+    </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
+import Bio from '../components/Bio.vue'
 export default {
-
+    components: {
+        Banner, Bio
+    }
 }
 </script>
 
 <style>
-
+.main {
+    @apply -z-10
+}
 </style>
