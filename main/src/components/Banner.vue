65fa10233da2541e6eacad5212c25e1949608216
Wahyu Pranata <wahyupranata131@gmail.com>
2022-05-29 22:41:48 +0800

    lower size of message and set default props value

--- main/src/components/Banner.vue
+++ main/src/components/Banner.vue
@@ -4,7 +4,7 @@
     <div class="absolute left-1/2 -translate-x-1/2 -bottom-24">
       <img :src="logo" alt="yorushika" class="rounded-xl w-48 shadow-md mx-auto">
       <div class="text-center">
-        <p class="font-lora text-xl mt-2 tracking-widest text-yoru-300">{{ message }}</p>
+        <p class="font-lora text-2xl mt-2 tracking-widest text-yoru-300">{{ message }}</p>
         <small class="font-nunitosans w-full tracking-widest text-yoru-400">{{ sub }}</small>
       </div>
     </div>
@@ -13,7 +13,14 @@
 
 <script>
 export default {
-  props: ['message', 'logo', 'sub']
+  props: {
+    logo: {
+      type: String,
+      default: 'src/assets/yorulogo.jpg'
+    },
+    message: String,
+    sub: String
+  }
 }
 </script>
 
