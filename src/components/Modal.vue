c2510418c58eaa1c55fad3100f299bf62acfd6e1
Wahyu Pranata <wahyupranata131@gmail.com>
2022-07-11 20:52:33 +0800

    add modal youtube embed

---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="modal-container" @click.self="$emit('modalClosed')">
+    <div class="modal">
+      <iframe
+        width="560"
+        height="315"
+        :src="'https://www.youtube.com/embed/' + modalVideoId"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    modalVideoId: String,
+  },
+};
+</script>
+
+<style>
+.modal-container {
+  @apply fixed top-0 left-0 z-10 h-screen w-screen overflow-hidden bg-slate-800/95;
+}
+.modal {
+  @apply absolute top-1/2 left-1/2 flex h-2/3 w-3/5 -translate-y-1/2 -translate-x-1/2 items-center justify-center rounded-md bg-yoru-200;
+}
+iframe {
+  @apply rounded-md;
+}
+</style>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,65 +1,102 @@
 <template>
   <div v-if="yt.length">
-        <div class="movie-container">
-            <div v-for="video in ytVideos" :key="video" class="video group">
-                <img :src="video.snippet.thumbnails.high.url" alt="Thumbnail" class="video-thumbnail">
-                <div class="overlay"></div>
-                <a :href="'https://www.youtube.com/watch?v=' + video.id.videoId" class="text-overlay">
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </div>
-                    <p>{{ video.snippet.title }}</p>
-                </a>
-            </div>
+    <div class="movie-container" :class="{ unscrollable: modalOpened }">
+      <div
+        v-for="video in ytVideos"
+        :key="video"
+        class="video group"
+        @click="openModal(video.id.videoId)"
+      >
+        <img
+          :src="video.snippet.thumbnails.high.url"
+          alt="Thumbnail"
+          class="video-thumbnail"
+        />
+        <div class="overlay"></div>
+        <div class="text-overlay">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="absolute top-1/2 left-1/2 h-12 w-12 -translate-x-1/2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <p>{{ video.snippet.title }}</p>
         </div>
+      </div>
     </div>
-    <p class="loader" v-else>
-        Loading movies data...
-    </p>
+  </div>
+  <p class="loader" v-else>Loading movies data...</p>
+  <Modal
+    v-if="modalOpened"
+    :modalVideoId="modalVideoId"
+    @modalClosed="modalOpened = false"
+  />
 </template>
 
 <script>
+import Modal from "./Modal.vue";
 export default {
-    data() {
-        return {
-            yt: [],
-            apiURL: 'http://localhost:3002/'
-        }
+  data() {
+    return {
+      modalOpened: false,
+      modalVideoId: "",
+      yt: [],
+      apiURL: "http://localhost:3002/",
+    };
+  },
+  components: {
+    Modal,
+  },
+  methods: {
+    openModal(id) {
+      this.modalOpened = true;
+      this.modalVideoId = id;
     },
-    mounted() {
-        fetch(`${ this.apiURL }`)
-            .then(data => data.json())
-            .then(res => this.yt = res.items)
-            .catch(err => console.log(err.message))
+  },
+  mounted() {
+    fetch(`${this.apiURL}`)
+      .then((data) => data.json())
+      .then((res) => (this.yt = res.items))
+      .catch((err) => console.log(err.message));
+  },
+  computed: {
+    ytVideos() {
+      return this.yt.filter((e) => e.id.kind == "youtube#video");
     },
-    computed: {
-        ytVideos() {
-            return this.yt.filter((e) => e.id.kind == "youtube#video")
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .movie-container {
-    @apply grid grid-cols-2 max-w-3xl mx-auto gap-8
+  @apply mx-auto grid max-w-3xl grid-cols-2 gap-8;
 }
 .video {
-    @apply relative
+  @apply relative;
 }
 .overlay {
-    @apply absolute inset-0 bg-black opacity-0 transition-all group-hover:opacity-75 rounded-xl
-  }
-.text-overlay {
-    @apply absolute bottom-0 left-1/2 w-full h-full font-bold text-center text-yoru-100 font-nunitosans opacity-0 transition-all duration-200 -translate-x-1/2 group-hover:bottom-4 group-hover:opacity-100
+  @apply absolute inset-0 rounded-xl bg-black opacity-0 transition-all group-hover:opacity-75;
 }
-.text-overlay div {
-    @apply absolute top-1/2 left-1/2 -translate-x-1/2
+.text-overlay {
+  @apply absolute bottom-0 left-1/2 h-full w-full -translate-x-1/2 text-center font-nunitosans font-bold text-yoru-100 opacity-0 transition-all duration-200 group-hover:bottom-4 group-hover:opacity-100;
 }
 .text-overlay p {
-    @apply absolute bottom-0 left-1/2 -translate-x-1/2 text-sm text-yoru-100 text-center
+  @apply absolute bottom-0 left-1/2 -translate-x-1/2 text-center text-sm text-yoru-100;
+}
+.unscrollable {
+  @apply h-full overflow-hidden;
 }
 </style>
